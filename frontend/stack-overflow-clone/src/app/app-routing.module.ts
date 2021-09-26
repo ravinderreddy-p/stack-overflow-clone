b85aeea9dcd22a77e20ee56c531aa438c1d85512
Ravi <ravinder9.reddy@gmail.com>
2021-09-26 21:16:02 +0530

    Enabled route guard service

--- frontend/stack-overflow-clone/src/app/app-routing.module.ts
+++ frontend/stack-overflow-clone/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard/:name', component: DashboardComponent },
-  { path: 'logout', component: LogoutComponent },
+  { path: 'dashboard/:name', component: DashboardComponent, canActivate: [RouteGuardService] },
+  { path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService] },
   { path: '**', component: ErrorComponent },
 ];
 
